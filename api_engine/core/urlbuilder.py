054121f4709650e1fe1ac6037fd66a9c4c983da6
Misha Bystrov <drudkhmisha@gmail.com>
2021-01-31 19:48:39 +0000

    Url builder class refactoring

--- api_engine/core/urlbuilder.py
+++ api_engine/core/urlbuilder.py
@@ -10,29 +10,41 @@ class WebUrlBuilder:
 
     def make_url(self, path: str) -> str:
         return f"{self.URL}{path}"
-    
-    def make_request(self, params):
-        """To delete"""
-        urlreq = Request(url)
-        content = urlopen(urlreq).read()
-        data = json.loads(content)
+
+    def __make_request(self, url, params = None):
+        """Create full url with parameters
+        Returns json with searching result with parameters 
+        """
+               
+       # data = urllib.parse.urlencode(params)
+
+        if params is not None:
+            params = urllib.parse.urlencode(params) #Convert [params] dictionary to http query
+            url = f"{url}?{params}"
+
+        data = urlopen(url).read()  #Opens url via urllib.request library
+        data = json.loads(data)  #Decoding json to a Python object [data]
         return data
 
     def search(
         self,
         q = '',
-        thumbsize = 'large',
-        category = '',
+        thumbsize = 'small',
+        category = None,
         page = 1,
-        ordering = '',
-        phrase = '',
-        tags = '',
-        period = '' 
+        ordering = None,
+        phrase: List[str] = None,
+        tags: List[str] = None,
+        period = None 
         ):
-        url = self.make_url('/search')
-                
-        params = {'search': q, 'page': page, 'thumbsize': thumbsize}
+        """Start searching with parameters
+        
+            Returns json with searching result with parameters"""
 
+        url = self.make_url('/search')
+
+        #There we fill [params] dictionary
+        params = {'search': q, 'page': page, 'thumbsize': thumbsize}
         if category is not None:
             params['category'] = category
         if ordering is not None:
@@ -44,36 +56,70 @@ class WebUrlBuilder:
         if period is not None and category is not None:
             params['period'] = period
 
-        data = urllib.parse.urlencode(params)
-        #data = data.encode('ascii')
-        #urlreq = Request(url, data)
+        data = self.__make_request(url, params)
 
-        final_url = url + '?' + data
-
-        content = urlopen(final_url).read()
-        output = json.loads(content)
-
-        return output
-
+        return data
 
     def stars(self):
-        url = self.make_url('/stars')
-        return url
+        """Get short pornstars list"""
 
-    def tags(self, *args):
+        url = self.make_url('/stars')
+
+        data = self.__make_request(url)
+
+        return data
+
+    def stars_detailed(self):
+        """Get detailed pornstars list"""
+
+        url = self.make_url('/stars_detailed')
+
+        data = self.__make_request(url)
+
+        return data
+
+    def video_by_id(self, id):
+        """Get video id"""
+
+        url = self.make_url('/video_by_id')
+
+        params = {'id': id}
+
+        data = self.__make_request(url, params)
+
+        return data
+
+    def is_video_active(self, id):
+    
+        url = self.make_url('/is_video_active')
+
+        params = {'id': id}
+
+        data = self.__make_request(url, params)
+
+        return data
+
+    def categories(self):
+        
+        url = self.make_url('/categories')
+
+        data = self.__make_request(url)
+
+        return data 
+
+    def tags(self, tags: List[str] = None):
+        
         url = self.make_url('/tags')
 
+        if tags is not None:
+            params['tags'] = ','.join(tags)
 
-    def stars(self):
-        url = self.make_url('/stars')
-        return url
+        data = self.__make_request(url, params)
 
-
-    def video_by_id(self):
-        url = self.make_url('/vide_by_id')
-        return
-
+        return data
 
 test = WebUrlBuilder()
 
-print(test.search(q = 'Anal'))
+tags = ('submissive', 'domination', 'ass fuck', 'fetish')
+
+print(test.search(q = 'Agent', page = 1, category = 'squirt', tags = tags))
