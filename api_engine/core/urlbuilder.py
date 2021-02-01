4342ea03b852bb39ba5955fdbafdac439b4ad75c
Misha Bystrov <drudkhmisha@gmail.com>
2021-02-01 12:34:14 +0000

    minor fixes regarding to linter

--- api_engine/core/urlbuilder.py
+++ api_engine/core/urlbuilder.py
@@ -1,49 +1,49 @@
-from typing import List, Optional
-from urllib.request import Request, urlopen
-import urllib.parse 
+from typing import List
+from urllib.request import urlopen
+import urllib.parse
 import json
 
 
 class WebUrlBuilder:
-    
     URL = "https://www.pornhub.com/webmasters"
 
     def make_url(self, path: str) -> str:
         return f"{self.URL}{path}"
 
-    def __make_request(self, url, params = None):
+    def __make_request(self, url, params=None):
         """Create full url with parameters
-        Returns json with searching result with parameters 
+        Returns json with searching result with parameters
         """
-               
-       # data = urllib.parse.urlencode(params)
+
+        # data = urllib.parse.urlencode(params)
 
         if params is not None:
-            params = urllib.parse.urlencode(params) #Convert [params] dictionary to http query
+            params = urllib.parse.urlencode(params)  # Convert [params]
+            # dictionary to http query
             url = f"{url}?{params}"
 
-        data = urlopen(url).read()  #Opens url via urllib.request library
-        data = json.loads(data)  #Decoding json to a Python object [data]
+        data = urlopen(url).read()  # Opens url via urllib.request library
+        data = json.loads(data)  # Decoding json to a Python object [data]
         return data
 
     def search(
         self,
-        q = '',
-        thumbsize = 'small',
-        category = None,
-        page = 1,
-        ordering = None,
+        q='',
+        thumbsize='small',
+        category=None,
+        page=1,
+        ordering=None,
         phrase: List[str] = None,
         tags: List[str] = None,
-        period = None 
-        ):
+        period=None
+            ):
         """Start searching with parameters
-        
+
             Returns json with searching result with parameters"""
 
         url = self.make_url('/search')
 
-        #There we fill [params] dictionary
+        # There we fill [params] dictionary
         params = {'search': q, 'page': page, 'thumbsize': thumbsize}
         if category is not None:
             params['category'] = category
@@ -90,7 +90,7 @@ class WebUrlBuilder:
         return data
 
     def is_video_active(self, id):
-    
+
         url = self.make_url('/is_video_active')
 
         params = {'id': id}
@@ -100,17 +100,17 @@ class WebUrlBuilder:
         return data
 
     def categories(self):
-        
+
         url = self.make_url('/categories')
 
         data = self.__make_request(url)
 
-        return data 
+        return data
 
     def tags(self, tags: List[str] = None):
-        
-        url = self.make_url('/tags')
 
+        url = self.make_url('/tags')
+        params = None
         if tags is not None:
             params['tags'] = ','.join(tags)
 
