0d386850904534be67eecbc62550b8c87ab24daa
Misha Bystrov <drudkhmisha@gmail.com>
2021-02-14 19:49:07 +0000

    added tests, schemas, fake responses for each function

--- api_engine/tests/search_unit_test.py
+++ api_engine/tests/search_unit_test.py
@@ -60,8 +60,111 @@ class TestGetData(unittest.TestCase):
 
         mock_search.return_value = mockfunc.urlopen.return_value
 
-        resp1 = Pornhub_api()
-        resp = resp1.search('sex')
+        resp = Pornhub_api()
+        resp = resp.search('sex')
+
+        self.assertIsInstance(resp, dict)
+        self.assertTrue(resp)
+
+        validate(resp, schema)
+
+    @patch('api_engine.core.pornhub_api.Pornhub_api.make_request')
+    def test_stars(self, mock_stars):
+
+        schema, response = get_schema_response("stars")
+
+        mockfunc = Mock()
+        mockfunc.urlopen.return_value = response
+
+        mock_stars.return_value = mockfunc.urlopen.return_value
+
+        resp = Pornhub_api()
+        resp = resp.search()
+
+        self.assertIsInstance(resp, dict)
+        self.assertTrue(resp)
+
+        validate(resp, schema)
+
+    @patch('api_engine.core.pornhub_api.Pornhub_api.make_request')
+    def test_stars_detailed(self, mock_stars_detailed):
+        schema, response = get_schema_response("stars_detailed")
+
+        mockfunc = Mock()
+        mockfunc.urlopen.return_value = response
+
+        mock_stars_detailed.return_value = mockfunc.urlopen.return_value
+
+        resp = Pornhub_api()
+        resp = resp.search()
+
+        self.assertIsInstance(resp, dict)
+        self.assertTrue(resp)
+
+        validate(resp, schema)
+
+    @patch('api_engine.core.pornhub_api.Pornhub_api.make_request')
+    def test_video_by_id(self, mock_video_by_id):
+        schema, response = get_schema_response("video_by_id")
+
+        mockfunc = Mock()
+        mockfunc.urlopen.return_value = response
+
+        mock_video_by_id.return_value = mockfunc.urlopen.return_value
+
+        resp = Pornhub_api()
+        resp = resp.search()
+
+        self.assertIsInstance(resp, dict)
+        self.assertTrue(resp)
+
+        validate(resp, schema)
+
+    @patch('api_engine.core.pornhub_api.Pornhub_api.make_request')
+    def test_is_video_active(self, mock_is_video_active):
+        schema, response = get_schema_response("is_video_active")
+
+        mockfunc = Mock()
+        mockfunc.urlopen.return_value = response
+
+        mock_is_video_active.return_value = mockfunc.urlopen.return_value
+
+        resp = Pornhub_api()
+        resp = resp.search()
+
+        self.assertIsInstance(resp, dict)
+        self.assertTrue(resp)
+
+        validate(resp, schema)
+
+    @patch('api_engine.core.pornhub_api.Pornhub_api.make_request')
+    def test_categories(self, mock_categories):
+        schema, response = get_schema_response("categories")
+
+        mockfunc = Mock()
+        mockfunc.urlopen.return_value = response
+
+        mock_categories.return_value = mockfunc.urlopen.return_value
+
+        resp = Pornhub_api()
+        resp = resp.search()
+
+        self.assertIsInstance(resp, dict)
+        self.assertTrue(resp)
+
+        validate(resp, schema)
+
+    @patch('api_engine.core.pornhub_api.Pornhub_api.make_request')
+    def test_tags(self, mock_tags):
+        schema, response = get_schema_response("tags")
+
+        mockfunc = Mock()
+        mockfunc.urlopen.return_value = response
+
+        mock_tags.return_value = mockfunc.urlopen.return_value
+
+        resp = Pornhub_api()
+        resp = resp.search()
 
         self.assertIsInstance(resp, dict)
         self.assertTrue(resp)
