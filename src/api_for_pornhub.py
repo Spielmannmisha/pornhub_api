d02716d21aabd77f9e2725ef3044c5ba747a4e01
Spielmannmisha <drudkhmisha@gmail.com>
2021-03-03 22:25:23 +0300

    changed structure from api_engine to src, renamed main file.py

--- src/api_for_pornhub.py
+++ src/api_for_pornhub.py
@@ -6,7 +6,13 @@ import urllib.parse
 import json
 
 
-class Pornhub_api:
+class Api_for_pornhub:
+    """This class creates functions that interact with pornhub.com
+
+    Returns:
+        functions: returns different api functions that interact with pornhub
+    """
+
     URL = "https://www.pornhub.com/webmasters"
 
     def make_url(self, path: str) -> str:
@@ -14,21 +20,20 @@ class Pornhub_api:
         return f"{self.URL}{path}"
 
     def make_request(self, url, params=None):
-        """Create full url with parameters
+        """[summary]
 
-        Keyword arguments:
-        url -- base url + category from specific method
-        params -- dictionary with parameters that's been set by user
+        Args:
+            url (String): unique url for each method
+            params (List, optional): search parameters. Defaults to None.
 
-        Return:
-        json as searching result with parameters
+        Returns:
+            json: returns json that depends on parameters
         """
 
         if params is not None:
             # Convert [params] dictionary to http query
             params = urllib.parse.urlencode(params)
             url = f"{url}?{params}"
-
         data = urlopen(url).read()  # Opens url via urllib.request library
         data = json.loads(data)  # Decoding json to a Python object [data]
         return data
@@ -46,25 +51,21 @@ class Pornhub_api:
             ):
         """Start searching with parameters
 
-        Keyword arguments:
-        q -- [string] user's query request. Default is ''.
-        thumbsize -- [string] could be small, medium, large,
-            small_hd, medium_hd, large_hd. Default is 'small'.
-        category -- [string]. Default is None.
-        page -- [int]. Default is 1
-        ordering -- [string] could be featured, newest, mostviewed,
-            rating. Defailt is None.
-        phrase -- [List]. Default is None.
-        tags -- [List]. Default is None.
-        period -- [string]. Default is None
+        Args:
+            q (str, optional): [description]. Defaults to ''.
+            thumbsize (str, optional): [description]. Defaults to 'small'.
+            category ([type], optional): [description]. Defaults to None.
+            page (int, optional): [description]. Defaults to 1.
+            ordering ([type], optional): [description]. Defaults to None.
+            phrase (List[str], optional): [description]. Defaults to None.
+            tags (List[str], optional): [description]. Defaults to None.
+            period ([type], optional): [description]. Defaults to None.
 
-        Return:
-        json as searching result with parameters
+        Returns:
+            json: returns json as searching result with parameters
         """
 
         url = self.make_url('/search')
-
-        # There we fill [params] dictionary
         params = {'search': q, 'page': page, 'thumbsize': thumbsize}
         if category is not None:
             params['category'] = category
@@ -76,67 +77,52 @@ class Pornhub_api:
             params['tags'] = ','.join(tags)
         if period is not None and category is not None:
             params['period'] = period
-
         data = self.make_request(url, params)
-
         return data
 
     def stars(self):
         """Get short pornstars list"""
 
         url = self.make_url('/stars')
-
         data = self.make_request(url)
-
         return data
 
     def stars_detailed(self):
         """Get detailed pornstars list"""
 
         url = self.make_url('/stars_detailed')
-
         data = self.make_request(url)
-
         return data
 
     def video_by_id(self, id):
         """Get video id"""
 
         url = self.make_url('/video_by_id')
-
         params = {'id': id}
-
         data = self.make_request(url, params)
-
         return data
 
     def is_video_active(self, id):
         """Check if video is active"""
 
         url = self.make_url('/is_video_active')
-
         params = {'id': id}
-
         data = self.make_request(url, params)
-
         return data
 
     def categories(self):
         """Get all possible categories"""
 
         url = self.make_url('/categories')
-
         data = self.make_request(url)
-
         return data
 
     def tags(self, tags: List[str] = None):
         """Get all tags"""
+
         url = self.make_url('/tags')
         params = None
         if tags is not None:
             params['tags'] = ','.join(tags)
-
         data = self.make_request(url, params)
-
         return data
