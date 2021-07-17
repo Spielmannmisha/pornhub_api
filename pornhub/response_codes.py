d67889ab4142d0ef6c2e3831f4290830ab929b7f
Misha Bystrov <drudkhmisha@gmail.com>
2021-07-17 15:25:19 +0000

    added type annotation for response_codes

--- pornhub/response_codes.py
+++ pornhub/response_codes.py
@@ -1,4 +1,7 @@
-def check_response(response):
+import json
+
+
+def check_response(response: json) -> None:
     faults = {"1001", "1002", "1003", "2001", "2002"}
     if "code" in response and response["code"] in faults:
         raise ValueError(f'{response["message"]}')
